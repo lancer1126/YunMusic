15ab2a801a3649b9e19bc0f5b73d6331628fb56a
lancer1126 <lancer1582@gmail.com>
2022-09-18 20:53:21 +0800

    使用Howler初步搭建音频流播放

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -1,4 +1,8 @@
 import { personFM } from "@/api/other";
+import { Howler, Howl } from "howler";
+import { getTrackDetail } from "@/api/track";
+import store from "@/store";
+import { isAccountLoggedIn } from "@/utils/auth";
 
 const excludeSaveKeys = ["_playing", "_personalFMLoading", "_personalFMNextLoading"];
 const UNPLAYABLE_CONDITION = {
@@ -37,11 +41,10 @@ export default class {
       enumerable: false,
     });
 
-    // init
     this._init();
 
-    window.yesplaymusic = {};
-    window.yesplaymusic.player = this;
+    window.yunmusic = {};
+    window.yunmusic.player = this;
   }
 
   // region getter/setter
@@ -60,16 +63,33 @@ export default class {
   get list() {
     return this.shuffle ? this._shuffledList : this._list;
   }
+  set list(list) {
+    this._list = list;
+  }
   get current() {
     return this.shuffle ? this._shuffledCurrent : this._current;
+  }
+  set current(current) {
+    if (this.shuffle) {
+      this._shuffledCurrent = current;
+    } else {
+      this._current = current;
+    }
   }
   get currentTrackId() {
     return this._currentTrack?.id ?? 0;
   }
+  get volume() {
+    return this._volume;
+  }
   // endregion
 
   // region 内部方法
+  /**
+   * 初始化数据
+   */
   _init() {
+    Howler.volume(this.volume);
     if (
       this._personalFMTrack.id === 0 ||
       this._personalFMNextTrack.id === 0 ||
@@ -82,17 +102,144 @@ export default class {
       });
     }
   }
+
+  /**
+   * 设置随机播放列表
+   */
   _shuffleTheList(firstTrackId = this.currentTrackId) {
     // todo 随机列表
     return firstTrackId;
   }
-  _replaceCurrentTrack(id, autoplay = true, nextUnplayable = UNPLAYABLE_CONDITION.PLAY_NEXT_TRACK) {
-    // todo 设置当前播放列表
-    return autoplay + nextUnplayable;
+
+  /**
+   * 设置当前播放的歌曲
+   */
+  _replaceCurrentTrack(id, autoplay = true, isNextUnplayable = UNPLAYABLE_CONDITION.PLAY_NEXT_TRACK) {
+    if (autoplay && this.currentTrack.name) {
+      // todo 播放进度条
+    }
+    getTrackDetail(id).then((data) => {
+      const track = data.songs[0];
+      this._currentTrack = track;
+      this._updateMediaSessionMetaData(track);
+      this._replaceCurTrackAudio(track, autoplay, true, isNextUnplayable);
+    });
+  }
+
+  /**
+   * 将歌曲信息更新到navigator中
+   */
+  _updateMediaSessionMetaData(track) {
+    if (!("mediaSession" in navigator)) {
+      return;
+    }
+    let artists = track.ar.map((a) => a.name);
+    const metadata = {
+      title: track.name,
+      artist: artists.join(","),
+      album: track.al.name,
+      artwork: [
+        {
+          src: track.al.picUrl + "?param=224y224",
+          type: "image/jpg",
+          sizes: "224x224",
+        },
+        {
+          src: track.al.picUrl + "?param=512y512",
+          type: "image/jpg",
+          sizes: "512x512",
+        },
+      ],
+      length: this.currentTrackDuration,
+      trackId: this.current,
+    };
+
+    navigator.mediaSession.metadata = new window.MediaMetadata(metadata);
+  }
+
+  /**
+   * 设置当前播放的歌曲的音频
+   */
+  _replaceCurTrackAudio(track, autoplay, isCachedNext, isNextUnplayable) {
+    return this._getAudioSource(track).then((source) => {
+      if (source) {
+        let replaced = false;
+        if (track.id === this.currentTrackId) {
+          this._playAudioSource(source, autoplay);
+          replaced = true;
+        }
+        if (isCachedNext) {
+          // todo 缓存下一首
+        }
+        return replaced;
+      } else {
+        store.dispatch("showToast", `无法播放 ${track.name}`);
+        switch (isNextUnplayable) {
+          case UNPLAYABLE_CONDITION.PLAY_NEXT_TRACK:
+            break;
+          case UNPLAYABLE_CONDITION.PLAY_PREV_TRACK:
+            break;
+          default:
+            store.dispatch("showToast", `undefined Unplayable condition: ${isNextUnplayable}`);
+        }
+        return false;
+      }
+    });
+  }
+
+  /**
+   * 获取音频源
+   */
+  _getAudioSource(track) {
+    return this._getAudioSourceFromNetease(track);
+  }
+
+  /**
+   * 播放音频
+   */
+  _playAudioSource(source, autoplay = true) {
+    Howler.unload();
+    this._howler = new Howl({
+      src: [source],
+      html5: true,
+      preload: true,
+      format: ["mp3", "flac"],
+    });
+
+    if (autoplay) {
+      this.play();
+    }
+  }
+
+  /**
+   * 从netease中获取音频源
+   */
+  _getAudioSourceFromNetease(track) {
+    if (isAccountLoggedIn()) {
+      // todo 登录后的获取
+    } else {
+      return new Promise((resolve) => {
+        resolve(`https://music.163.com/song/media/outer/url?id=${track.id}`);
+      });
+    }
+  }
+
+  // endregion
+
+  // region 播放器动作
+  /**
+   * 播放
+   */
+  play() {
+    if (this._howler?.playing()) return;
+    this._howler?.play();
   }
   // endregion
 
   // region 外部方法
+  /**
+   * 将播放器信息存入localStorage
+   */
   saveSelfToLocalStorage() {
     let player = {};
     for (let [key, value] of Object.entries(this)) {
@@ -101,6 +248,10 @@ export default class {
     }
     localStorage.setItem("player", JSON.stringify(player));
   }
+
+  /**
+   * 设置或替换当前的播放列表
+   */
   replacePlaylist(trackIds, sourceId, sourceType, autoPlayTrackId = "first") {
     this._isPersonalFM = false;
     if (!this._enabled) this._enabled = true;
